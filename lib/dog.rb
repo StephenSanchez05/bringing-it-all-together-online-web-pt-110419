5a41091e9f41f71133c53ab77a38ed42f0871d8c
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-11 20:40:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,5 +10,14 @@ class Dog
   
   def self.create_table
     sql = SQL
-    
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.
 end 
