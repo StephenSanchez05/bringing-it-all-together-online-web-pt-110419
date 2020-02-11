05bfe58f27446d37f0cc87662377932c88225cef
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-11 20:50:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,6 +41,9 @@ class Dog
   def update
     sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
-  end
+  
+  
+  def self.create
+    
   
 end 
